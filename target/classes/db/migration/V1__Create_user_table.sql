81f61779ab1811b710f7ca6cfee7f66a5951240d
jianqi <jianqi.zhao.xian@gmail.com>
2022-02-22 23:45:11 +1100

    add .gitignore

--- target/classes/db/migration/V1__Create_user_table.sql
+++
@@ -1,9 +0,0 @@
-create table USER
-(
-    ID  bigint primary key auto_increment,
-    NAME varchar(100),
-    TEL  varchar(20) unique,
-    AVATAR_URL varchar(300),
-    CREATED_AT timestamp,
-    MODIFIED_AT timestamp
-)
